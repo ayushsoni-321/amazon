40e194f39d632c5f50c82c581a78d71058b84b5a
ayushsoni-321 <ayushsoni12072000@gmail.com>
2022-04-26 18:51:34 +0530

    Initial commit

---
+++ src/Checkout.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Checkout.css";
+
+function Checkout() {
+    return <div className="checkout">
+        <h1>Smash the like button</h1>
+    </div>
+}
+export default Checkout;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCuYtl2rihvcggd3ERTJgn7NAAmVTvyb9M",
+    authDomain: "copy-9daa7.firebaseapp.com",
+    projectId: "copy-9daa7",
+    storageBucket: "copy-9daa7.appspot.com",
+    messagingSenderId: "763607951270",
+    appId: "1:763607951270:web:a755926596ee6a08d65167",
+    measurementId: "G-YQQCHYQ00D"
+  };
