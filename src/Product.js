4a9561c914f2db75c9ca8c4ed9d70762ac4480f1
ayushsoni-321 <ayushsoni12072000@gmail.com>
2022-04-27 18:43:45 +0530

    Checkout page

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Product.css";
-function Product({title, image, price, rating}) {
+import { useStateValue } from "./StateProvider";
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log("this is the basket >>>", basket);
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
     return(
     <div className="product">
         <div className="product__info">
@@ -13,13 +30,13 @@ function Product({title, image, price, rating}) {
                 {Array(rating)
                 .fill()
                 .map((_, i) => (
-                    <p></p>
+                    <p>⭐</p>
                 ))}
             </div>
         </div>
         <img src={image}
         alt=""/>
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
     </div>
     );
 }
