2db60ffb0821ea3b9f14b8758838b704d0d97e26
ayushsoni-321 <ayushsoni12072000@gmail.com>
2022-04-30 18:39:56 +0530

    Created payment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./Header";
 import Home from "./Home";
@@ -6,8 +6,35 @@ import { BrowserRouter as Router, Switch, Route }
 from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from './Payment';
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe('pk_test_51KuDaRSBItOabOhKw51mLz7gWonvF6quBQg4zgbxMO0oqvjEzpQy3LJLZoP2N6qLIeJcUfVHML6KWnoRN5uVun1c00dCCTuCBH');
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+      if (authUser) {
+        // the user just logged in / the user was logged in 
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
   return (
     <Router>
     <div className="app">
@@ -21,6 +48,12 @@ function App() {
         <Route path="/checkout">
           <Header />
           <Checkout />
+        </Route>
+        <Route path="/payment">
+          <Header />
+          <Elements stripe={promise}>
+             <Payment />  
+          </Elements>
         </Route>
         <Route path="/">
             <Header />
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,22 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCuYtl2rihvcggd3ERTJgn7NAAmVTvyb9M",
-    authDomain: "copy-9daa7.firebaseapp.com",
-    projectId: "copy-9daa7",
-    storageBucket: "copy-9daa7.appspot.com",
-    messagingSenderId: "763607951270",
-    appId: "1:763607951270:web:a755926596ee6a08d65167",
-    measurementId: "G-YQQCHYQ00D"
-  };
+  apiKey: "AIzaSyCuYtl2rihvcggd3ERTJgn7NAAmVTvyb9M",
+  authDomain: "copy-9daa7.firebaseapp.com",
+  projectId: "copy-9daa7",
+  storageBucket: "copy-9daa7.appspot.com",
+  messagingSenderId: "763607951270",
+  appId: "1:763607951270:web:a755926596ee6a08d65167",
+  measurementId: "G-YQQCHYQ00D"
+};
+
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
